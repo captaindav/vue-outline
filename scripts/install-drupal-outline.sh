f8974fe11e57d248098c9be1626983c52bc8ae58
Dave Connerth <dave@answerglobal.com>
2020-04-10 14:13:32 -0400

    Lando changes.

--- scripts/install-drupal-outline.sh
+++
@@ -1,7 +0,0 @@
-cd ../web
-composer require drupal/core:dev
-composer require drupal/admin_toolbar
-composer require cweagans/composer-patches
-composer require drupal/ctools
-composer require drupal/dynamic_entity_reference
-composer require drupal/outline
