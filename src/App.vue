1b14ee5e38bba8cc8c73c441ad14304d6cd1efa9
IT Solution <itsolution0820@Gmail.com>
2020-04-13 11:52:55 -0400

    clean up

--- src/App.vue
+++ src/App.vue
@@ -38,19 +38,19 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <ApolloExample/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import ApolloExample from './components/ApolloExample';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    ApolloExample,
   },
 
   data: () => ({
