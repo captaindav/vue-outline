c9e74455588b0be2d2e0801ec9af5db1240a5c9b
captaindav <dave@answerglobal.com>
2020-08-22 18:13:17 -0400

    Cleanup.

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -3,7 +3,6 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const httpLink = createHttpLink({
-  // uri: 'https://localhost:32778',
   uri: 'http://drupal-outline.lndo.site/outline-graphql',
 });
 
