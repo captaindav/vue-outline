f5c0f786bd190b978605f5a65e08172a9b04086a
AMajesticPotatoe <amajesticpotatoe@gmail.com>
2020-10-25 18:39:34 -0400

    refactor: update app-bar styling

--- src/App.vue
+++ src/App.vue
@@ -15,21 +15,24 @@
       app
       clipped-left
       color="primary"
+      height="56"
       dark
     >
       <v-toolbar-title>
-        <h1>Drupal Vue Outline Demo</h1>
+        <h1 class="text-h6">Drupal Vue Outline Demo</h1>
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-menu offset-y>
         <template v-slot:activator="{ on }">
-          <v-toolbar-items> 
-            <v-btn text v-on="on">
-              <v-icon>mdi-menu</v-icon>
-            </v-btn>
-          </v-toolbar-items>
+          <v-btn
+            icon
+            left
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
         </template>
         <v-list>
           <v-list-item @click="dialog = true">
@@ -69,14 +72,12 @@
   };
 </script>
 
-<style>
-  .splitpanes--vertical > .splitpanes__splitter {
-    min-width: 4px;
-    background: rgba(0,0,0,.2);
-  }
+<style lang="sass">
+  .splitpanes--vertical > .splitpanes__splitter
+    min-width: 4px
+    background: rgba(0,0,0,.2)
 
-  .splitpanes--horizontal > .splitpanes__splitter {
-    min-height: 4px;
-    background: rgba(0,0,0,.2);
-  }
+  .splitpanes--horizontal > .splitpanes__splitter
+    min-height: 4px
+    background: rgba(0,0,0,.2)
 </style>
