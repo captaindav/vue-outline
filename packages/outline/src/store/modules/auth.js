b46fb130d08c72749cc00d797ddff13eb18c32b2
amajesticpotatoe <amajesticpotatoe@gmail.com>
2020-12-10 15:14:11 -0500

    feat(store): rename drawer to bookmark, add auth

---
+++ packages/outline/src/store/modules/auth.js
@@ -0,0 +1,24 @@
+import { make } from 'vuex-pathify'
+
+const state = {
+  activeServers: [0],
+  availableServers: [
+    { sid: 1, name: 'Server 1' },
+    { sid: 2, name: 'Server 2', disabled: true },
+    { sid: 3, name: 'Server 3', disabled: true},
+  ],
+}
+
+const mutations = make.mutations(state)
+
+const actions = make.actions(state)
+
+const getters = make.getters(state)
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters,
+}
