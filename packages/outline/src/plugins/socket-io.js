1173622214a7e5359a946bc55f5d8203288b2863
amajesticpotatoe <amajesticpotatoe@gmail.com>
2020-12-10 14:11:48 -0500

    refactor(socket-io): disable reconnect

--- packages/outline/src/plugins/socket-io.js
+++ packages/outline/src/plugins/socket-io.js
@@ -4,6 +4,8 @@ import { io } from 'socket.io-client'
 import store from './../store'
 
 const host = 'http://localhost:8090'
-const socket = io(host)
+const socket = io(host, {
+  reconnection: false
+})
 
 Vue.use(VueSocketIOExt, socket, { store })
