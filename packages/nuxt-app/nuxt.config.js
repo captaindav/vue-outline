f917352afa5a6a261bae5c97b17a8fcc1c3c9d23
amajesticpotatoe <amajesticpotatoe@gmail.com>
2021-04-19 21:59:43 -0400

    feat: init nuxt package with druxt and vuetify

---
+++ packages/nuxt-app/nuxt.config.js
@@ -0,0 +1,10 @@
+export default {
+  buildModules: [
+    '@nuxtjs/vuetify',
+    'druxt'
+  ],
+  druxt: {
+    baseUrl: 'http://drupal-outline.lndo.site/outline-graphql'
+  },
+  vuetify: {},
+}
---
+++ packages/nuxt-app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-main>
+    <v-alert type="info">
+      Testing Nuxt Environment
+    </v-alert>
+  </v-main>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+  }
+</script>
