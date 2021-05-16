9d55647a67ada8356a175515a8c592eaf5c7a8f8
amajesticpotatoe <amajesticpotatoe@gmail.com>
2021-05-16 18:31:48 -0400

    chore: update package and lint

--- packages/outline/.eslintrc.js
+++ packages/outline/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  root: true,
+  root: false,
   env: {
     node: true
   },
@@ -11,7 +11,7 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off'
   },
 }
